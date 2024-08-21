139b6b2ad954cda4a6ad14e6b726f0174720549e
updaun <updauney@daum.net>
2024-08-21 13:01:38 +0900

    chore: django-bootstrap5 setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third-party
     "debug_toolbar",
+    "django_bootstrap5",
     # Local
     "accounts",
 ]
@@ -71,7 +72,9 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
