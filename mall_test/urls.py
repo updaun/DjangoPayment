1f7b54cd7b38112f87c5d6a8e511acf97cff3327
updaun <updauney@daum.net>
2024-08-23 20:32:28 +0900

    chore: iamport setting

--- mall_test/urls.py
+++ mall_test/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from mall_test import views
+
+urlpatterns = [
+    path("payment/new/", views.payment_view, name="payment_new"),
+]
--- mall_test/views.py
+++ mall_test/views.py
@@ -1,1 +1,5 @@
 from django.shortcuts import render
+
+
+def payment_view(request):
+    return render(request, "mall_test/payment_form.html")
