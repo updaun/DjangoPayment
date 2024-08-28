ab7fe98c0ab692b035e8cd4a23c48e59a89596d2
updaun <updauney@daum.net>
2024-08-28 20:50:24 +0900

    chore: accounts signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from accounts.models import User
+from accounts.forms import SignupForm
 
-# Create your views here.
+
+signup = CreateView.as_view(
+    model=User,
+    form_class=SignupForm,
+    template_name="accounts/signup_form.html",
+    success_url=reverse_lazy("login"),
+)
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def profile(request):
+    pass
