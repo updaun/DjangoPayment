6538234be64340325bdecd1588bb7f27550e7649
updaun <updauney@daum.net>
2024-09-04 20:10:33 +0900

    chore: search and search pagination

--- mall/views.py
+++ mall/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from mall.models import Product
 from django.views.generic import ListView
@@ -7,6 +9,13 @@ class ProductListView(ListView):
     model = Product
     queryset = Product.objects.all().select_related("category")
     paginate_by = 4
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get("query", "")
+        if query:
+            qs = qs.filter(name__icontains=query)
+        return qs
 
 
 product_list = ProductListView.as_view()
