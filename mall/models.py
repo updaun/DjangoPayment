2d51c378d3524ba9caf383b2c4e0c189f315434c
updaun <updauney@daum.net>
2024-09-05 20:16:18 +0900

    chore: cart model

--- mall/models.py
+++ mall/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models.constraints import UniqueConstraint
 
 
 class Category(models.Model):
@@ -41,3 +43,33 @@ class Product(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "상품"
         ordering = ["-pk"]
+
+
+class CartProduct(models.Model):
+    user = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.CASCADE,
+        db_constraint=False,
+        related_name="cart_product_set",
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+        validators=[MinValueValidator(1)],
+    )
+
+    def __str__(self):
+        return f"<{self.pk}> {self.product.name} x {self.quantity}"
+
+    class Meta:
+        verbose_name_plural = verbose_name = "장바구니 상품"
+        constraints = [
+            UniqueConstraint(
+                fields=["user", "product"],
+                name="unique_user_product",
+            ),
+        ]
