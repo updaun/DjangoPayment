42c8324463252d1ea40f90c4be0761c117032365
updaun <updauney@daum.net>
2024-09-09 19:21:58 +0900

    chore: order model definition

--- mall/models.py
+++ mall/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.db.models.constraints import UniqueConstraint
+from uuid import uuid4
 
 
 class Category(models.Model):
@@ -73,3 +74,46 @@ class CartProduct(models.Model):
                 name="unique_user_product",
             ),
         ]
+
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        REQUESTED = "requested", "주문요청"
+        FAILED_PAYMENT = "failed_payment", "결제실패"
+        PAID = "paid", "결제완료"
+        PREPARED_PRODUCT = "prepared_product", "상품준비중"
+        SHIPPED = "shipped", "배송중"
+        DELIVERED = "delivered", "배송완료"
+        CANCELED = "canceled", "주문취소"
+
+    uid = models.UUIDField(default=uuid4, editable=False, unique=True)
+    user = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    total_amount = models.PositiveIntegerField("결제금액")
+    status = models.CharField(
+        "진행상태",
+        choices=Status.choices,
+        default=Status.REQUESTED,
+        max_length=20,
+        db_index=True,
+    )
+    product_set = models.ManyToManyField(
+        Product,
+        through="OrderedProduct",
+        blank=False,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderedProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, db_constraint=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, db_constraint=False)
+    name = models.CharField("상품명", max_length=100, help_text="주문 시점의 상품평을 저장합니다.")
+    price = models.PositiveIntegerField("가격", help_text="주문 시점의 가격을 저장합니다.")
+    quantity = models.PositiveIntegerField("수량")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
